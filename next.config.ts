d5fb0ab739f90fe0bfb1f1bfbedafb78af115ed4
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-03-24 12:52:48 +0600

    update next.config file

--- next.config.ts
+++ next.config.ts
@@ -4,18 +4,22 @@ const nextConfig: NextConfig = {
   /* config options here */
 
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**", // Allow all images from Unsplash
-      },
-      {
-        protocol: "https",
-        hostname: "unsplash.com",
-        pathname: "/**", // Allow all images from Unsplash
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "images.unsplash.com",
+    //     pathname: "/**", // Allow all images from Unsplash
+    //   },
+    //   {
+    //     protocol: "https",
+    //     hostname: "unsplash.com",
+    //     pathname: "/**", // Allow all images from Unsplash
+    //   },
+    // ],
+    domains:[
+        "images.unsplash.com",
+        "unsplash.com"
+    ]
   },
 };
 
