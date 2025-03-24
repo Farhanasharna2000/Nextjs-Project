5fe61a4b3d4d6150b601be931b0b12f66ad8c4df
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-03-24 21:01:00 +0600

    added css modules in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './page.module.css'
 export const metadata ={
     title:"About Page",
     description:"This is the about page"
@@ -6,7 +7,7 @@ export const metadata ={
 const About = () => {
     return (
         <div>
-           About 
+           <h1 className={`${styles.title}`}>About NextJS Project</h1> 
         </div>
     );
 };
